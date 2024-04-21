37ab19cd41fe9239566f602df78f9ed7d0b902f3
jooahnpark <joooahn@naver.com>
2024-04-22 02:49:49 +0900

    손절 기능 추가

--- BackTesting.py
+++ BackTesting.py
@@ -14,12 +14,12 @@ def merge_candle(candle_30min, candle_5min):
 
     # 5분봉, 30분봉 합치기
     candle_30min['key'] = np.arange(len(candle_30min))
-    tmpKey = np.repeat(np.arange(len(candle_5min) // 6), 6)
+    tmp_key = np.repeat(np.arange(len(candle_5min) // 6), 6)
 
     # 임시 키 개수가 원본 행 개수와 맞지 않을 경우
-    if tmpKey.size < len(candle_5min):
-        tmpKey = np.append(tmpKey, np.repeat(tmpKey[-1], len(candle_5min) - tmpKey.size))
-    candle_5min['key'] = tmpKey
+    if tmp_key.size < len(candle_5min):
+        tmp_key = np.append(tmp_key, np.repeat(tmp_key[-1], len(candle_5min) - tmp_key.size))
+    candle_5min['key'] = tmp_key
 
     # Reset the current index 'time' and keep it as a column
     candle_30min.reset_index(inplace=True)
@@ -39,31 +39,50 @@ def calculate_ror(sell_price, buy_price, fee, leverage):
     return 1 - (1 - sell_price * (1 - fee) / buy_price * (1 + fee)) * leverage
 
 
-def set_ror(candle_info, leverage, k):
+def set_ror(candle_info, leverage, fee, loss_cut, k):
     # 직전 30분봉 기준 range, target 계산
+    # 즉, 이번 5분봉 마감이 직전 30분봉을 k% 장악하는 양봉일 때 매수, 이번 30분봉 마감 시 매도
     candle_info['range'] = (candle_info['high_x'] - candle_info['low_x']) * k
     candle_info['target'] = candle_info['open_x'] + candle_info['range'].shift(6)
 
-    # 5분봉 종가 마감이 target 보다 높으면 매수
-    # 즉, 이번 5분봉 마감이 직전 30분봉을 k% 장악하는 양봉일 때 매수, 이번 30분봉 마감 시 매도
-    fee = 0.0004
-    candle_info['buy'] = np.where(candle_info['close_y'] > candle_info['target'], 1, 0)
-    candle_info['ror'] = np.where(candle_info['close_y'] > candle_info['target'],
-                                  calculate_ror(candle_info['close_x'], candle_info['close_y'], fee, leverage),
-                                  1)
-    # 30분에 1번 거래하므로 30분봉 기준으로 누적 수익률 계산
-    bought = True
+    # 거래 진행 정보
+    candle_info['buy'] = 0
+    candle_info['sell'] = 0
+    candle_info['ror'] = 1.0
+    candle_info['stop_loss'] = 0
+    stop_loss = 1 - loss_cut / 100
+    # 언제 buy했는지 기록하는 index
+    buy_index = 0
+    buy_price = 0
+    # 이번 30분봉 내 매수 여부 판별하는 flag
+    bought = False
+
+    # 거래 진행
     for i in range(len(candle_info)):
+        current_price = candle_info.iloc[i]['close_y']
+        target_price = candle_info.iloc[i]['target']
         # 30분봉 시작 시
         if i % 6 == 0:
             bought = False
-        # 30분봉 시작 후 최초 매수일 경우
-        if candle_info['buy'].iloc[i] == 1 and not bought:
+        # 5분봉 종가 마감이 target 보다 높으면 매수
+        if not bought and current_price > target_price:
+            candle_info.loc[i, 'buy'] = 1
             bought = True
-        # 30분봉 시작 후 최초 매수가 아닐 경우 매수 신호 제거
-        elif candle_info['buy'].iloc[i] == 1 and bought:
-            candle_info['buy'].iloc[i] = 0
-            candle_info['ror'].iloc[i] = 1
+            buy_index = i
+            buy_price = current_price
+            continue
+        # 2% 하락 시 손절
+        if bought and calculate_ror(current_price, buy_price, fee, leverage) <= stop_loss:
+            candle_info.loc[i, 'sell'] = 1
+            candle_info.loc[i, 'stop_loss'] = 1
+            candle_info.loc[i, 'ror'] = calculate_ror(current_price, buy_price, fee, leverage)
+            bought = False
+            continue
+        # 30분봉 마감 시 매도
+        if bought and i % 6 == 5:
+            candle_info.loc[i, 'sell'] = 1
+            candle_info.loc[i, 'ror'] = calculate_ror(current_price, buy_price, fee, leverage)
+            continue
 
     # 누적 수익률
     candle_info['hpr'] = candle_info['ror'].cumprod()
@@ -83,7 +102,7 @@ merged_candle = merge_candle(candle_30min, candle_5min)
 #     print("전략 수익률 : " + str(result[0]))
 #     print("보유 수익률 : " + str(result[1]))
 
-result = set_ror(merged_candle, 10, 0.5)
+result = set_ror(merged_candle, 10, 0.0004, 10, 0.5)
 print("k값 : " + str(0.5))
-print("전략 수익률 : " + str(result[0]))
-print("보유 수익률 : " + str(result[1]))
+print("전략 적용 시 수익률 : " + str(result[0]))
+print("보유 시 수익률 : " + str(result[1]))
